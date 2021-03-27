13c2c529aa456ff006ce5d6df90744637c2c07f9
Maxim Ivanov <f1sst36@gmail.com>
2021-03-27 18:32:41 +0300

    try to fix mail

--- src/app/ship/mail/index.ts
+++ src/app/ship/mail/index.ts
@@ -1,13 +1,12 @@
 import fs from "fs";
 import nodemailer from "nodemailer";
 
-export const sendMail = async (
-    recipient: String,
-    subject: String,
-    html: String,
-) => {
+export const sendMail = async (recipient: String, subject: String, html: String) => {
     let transporter = nodemailer.createTransport({
         service: "gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.MAIL_LOGIN,
             pass: process.env.MAIL_PASSWORD,
@@ -22,8 +21,8 @@ export const sendMail = async (
     };
 
     transporter.sendMail(mailOptions, (err, data) => {
-        console.log("sendMailError", err);
-        
+        console.log("sendMail", err);
+
         if (err) fs.appendFile(__dirname + "/logs/error.txt", JSON.stringify(err) + "\n", () => {});
         if (data)
             fs.appendFile(__dirname + "/logs/success.txt", JSON.stringify(data) + "\n", () => {});
