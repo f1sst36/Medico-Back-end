6a6c3ac43bd5a80c4dd5ae3c457c874623fba9a8
Maxim Ivanov <f1sst36@gmail.com>
2021-04-22 16:28:10 +0300

    changed readJson helper function

--- src/app/ship/helper/jsonReader.ts
+++ src/app/ship/helper/jsonReader.ts
@@ -2,9 +2,5 @@ import fs from 'fs';
 import path from 'path';
 
 export const readJson = (absolutePathToFile: string) => {
-    return fs
-        .readFileSync(path.resolve(absolutePathToFile))
-        .toString()
-        .split(/(?={")/)
-        .map((x) => JSON.parse(x))[0];
+    return JSON.parse(fs.readFileSync(path.resolve(absolutePathToFile)).toString());
 };
