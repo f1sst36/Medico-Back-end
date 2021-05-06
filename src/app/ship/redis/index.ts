6b1d8a7d5487e216bf670e5d3db67db2f6130c70
Maxim Ivanov <f1sst36@gmail.com>
2021-05-06 18:52:39 +0300

    remote redis connection

--- src/app/ship/redis/index.ts
+++ src/app/ship/redis/index.ts
@@ -1,4 +1,8 @@
 import redis from 'redis';
 
-export const client = redis.createClient(process.env.REDIS_PORT);
+// Локальное подключение
+// export const client = redis.createClient(process.env.REDIS_PORT);
 
+// Удаленное подключение на аддон хероку
+// export const client = redis.createClient(process.env.REDISCLOUD_URL, { no_ready_check: true });
+export const client = redis.createClient(process.env.REDISCLOUD_URL);
