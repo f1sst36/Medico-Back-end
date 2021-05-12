d6410ae336f5943b640c8b113fb9e94dec9960b5
f1sst <f1sst36@gmail.com>
2021-05-12 15:50:28 +0300

    file storage fix

--- src/app/ship/helper/FileStorage.ts
+++ src/app/ship/helper/FileStorage.ts
@@ -3,7 +3,10 @@ import crypto from 'crypto';
 export class FileStorage {
     public static moveFile = async (file: any): Promise<string> => {
         const uploadPath = 'src/app/ship/storage/files/';
-        const newFileName = crypto.randomBytes(20).toString('hex');
+        const sliptResult: Array<string> = file.name.split('.');
+
+        const newFileName =
+            crypto.randomBytes(20).toString('hex') + '.' + sliptResult[sliptResult.length - 1];
         await file.mv(uploadPath + newFileName);
 
         return '/storage/files/' + newFileName;
